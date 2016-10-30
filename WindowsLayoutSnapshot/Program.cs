46785e0e5c6dcbc164bcf1def6bdd9c6f1803c48
SVargh <steve@varghese.de>
2016-10-30 17:05:05 +0100

    auto signed test

--- WindowsLayoutSnapshot/Program.cs
+++ WindowsLayoutSnapshot/Program.cs
@@ -11,7 +11,7 @@ namespace WindowsLayoutSnapshot {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Console.Out.WriteLine("Press any key to continue!sxxxs");
+            Console.Out.WriteLine("Press any key to continue!sxxxss");
             Console.ReadKey();
             Application.Run(new TrayIconForm());            
         }
